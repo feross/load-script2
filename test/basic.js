58f772fb377e016ec3503439aaf08c8a5a0708c3
delvedor <tommydelved@gmail.com>
2017-02-22 00:11:02 +0100

    Updated test

--- test/basic.js
+++ test/basic.js
@@ -6,12 +6,26 @@ window.log = function (msg) {
   lastMessage = msg
 }
 
-test('success', function (t) {
-  t.plan(2)
+test('success head', function (t) {
+  t.plan(3)
 
+  var headChildren = document.head.children.length
   load('test/lib/hello.js', function (err) {
     t.error(err)
     t.equal(lastMessage, 'Hello world')
+    t.equal(document.head.children.length, headChildren + 1)
+    lastMessage = undefined
+  })
+})
+
+test('success body', function (t) {
+  t.plan(3)
+
+  var bodyChildren = document.body.children.length
+  load('test/lib/hello.js', { body: true }, function (err) {
+    t.error(err)
+    t.equal(lastMessage, 'Hello world')
+    t.equal(document.body.children.length, bodyChildren + 1)
     lastMessage = undefined
   })
 })
