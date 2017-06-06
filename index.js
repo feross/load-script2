2aca771fc756377d1c7e189e2935b73c342ac73c
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2017-06-06 16:18:52 +0200

    feat: use promise
    modern browsers have promises
    You could also add a link to https://cdn.polyfill.io/v2/polyfill.min.js?features=Promise for those who need it.
    
    This is a breaking change.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 module.exports = load
 
-function load (src, cb) {
-  var head = document.head || document.getElementsByTagName('head')[0]
-  var script = document.createElement('script')
+function load (src) {
+  return new Promise(function (resolve, reject) {
+    var head = document.head || document.getElementsByTagName('head')[0]
+    var script = document.createElement('script')
 
-  script.type = 'text/javascript'
-  script.async = true
-  script.src = src
+    script.type = 'text/javascript'
+    script.async = true
+    script.src = src
 
-  if (cb) {
     script.onload = function () {
       script.onerror = script.onload = null
-      cb(null, script)
+      resolve(script)
     }
     script.onerror = function () {
       script.onerror = script.onload = null
-      cb(new Error('Failed to load ' + src), script)
+      reject(new Error('Failed to load ' + src))
     }
-  }
 
-  head.appendChild(script)
+    head.appendChild(script)
+  })
 }
