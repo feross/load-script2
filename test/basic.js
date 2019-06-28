fb299611e5443175b5fdde4e4718bd5b0d6e4a1a
Feross Aboukhadijeh <feross@feross.org>
2019-06-28 15:37:52 -0700

    fix tests

--- test/basic.js
+++ test/basic.js
@@ -7,19 +7,22 @@ window.log = function (msg) {
 }
 
 test('success', function (t) {
-  t.plan(2)
+  t.plan(1)
 
-  load('test/lib/hello.js', function (err) {
-    t.error(err)
+  load('test/lib/hello.js').then(function () {
     t.equal(lastMessage, 'Hello world')
     lastMessage = undefined
+  }, function () {
+    t.fail('script should inject successfully')
   })
 })
 
 test('nonexistant script', function (t) {
   t.plan(1)
 
-  load('nonexistant.js', function (err) {
+  load('nonexistant.js').then(function () {
+    t.fail('non existant script should not load')
+  }, function (err) {
     t.ok(err instanceof Error)
   })
 })
@@ -27,7 +30,9 @@ test('nonexistant script', function (t) {
 test('throw', function (t) {
   t.plan(1)
 
-  load('test/lib/throw.js', function (err) {
-    t.error(err)
+  load('test/lib/throw.js').then(function () {
+    t.ok('loaded')
+  }, function () {
+    t.fail('thrown exception in script should not affect script promise')
   })
 })
